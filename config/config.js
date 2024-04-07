0ebe7073d340b2ca9b5c96fa6dc21730ba37f5ad
Shivam Tiwari <shivamtiwari.57.2020@gmail.com>
2024-04-07 17:02:06 +0530

    Config change

--- config/config.js
+++ config/config.js
@@ -4,8 +4,8 @@ console.log(process.env.OBJECT_VAL)
 const objectval = JSON.parse(process.env.OBJECT_VAL);
 console.log(objectval)
 const defaultConfig = objectval['development'];
-const environment='development';
-// const environment = "production"
+// const environment='development';
+const environment = "production"
 
 const environmentConfig = objectval[environment];
 // console.log(environmentConfig);
